4de2780188a5237a7395e2a2dd5f1ca5fed70742
ad56311014 <zhuwc@hotmail.com>
2017-04-20 12:56:33 +0800

    Create plot1.R

---
+++ plot1.R
@@ -0,0 +1,5 @@
+source('project1data.R')
+data <- dataf()
+png(filename='plot1.png',width = 480, height = 480, units='px')
+hist(data$Global_active_power,xlab='Global Active Power(kilowatts)',col='red',main='Global Active Power')
+dev.off()
